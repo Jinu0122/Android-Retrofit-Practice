c3c45d97484d13fdff30858579a5d281b5b83500
Jinwoo <jnu@Jinwooui-MacBookPro.local>
2020-08-17 23:14:35 +0900

    메테리얼 테마 사용한 화면구성

---
+++ app/src/main/java/com/jnu/unsplash_app_tutorial/MainActivity.kt
@@ -0,0 +1,92 @@
+package com.jnu.unsplash_app_tutorial
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import com.jnu.unsplash_app_tutorial.utlis.Constants.TAG
+import com.jnu.unsplash_app_tutorial.utlis.SERCH_TYPE
+import com.jnu.unsplash_app_tutorial.utlis.onMyTextChange
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.layout_button_search.*
+
+class MainActivity : AppCompatActivity() {
+
+    private var currentSearchType: SERCH_TYPE = SERCH_TYPE.PHOTO
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Log.d(TAG, "MainActivity - onCreate() called")
+
+
+        // 레디오 그룹 가져오기
+        search_term_radio_group.setOnCheckedChangeListener { _, checkedId ->
+
+
+            // switch 문
+            when (checkedId) {
+                R.id.photo_search_radio_btn -> {
+                    Log.d(TAG, "사진검색 버튼 클릭!")
+                    search_term_text_layout.hint = "사진검색"
+                    search_term_text_layout.startIconDrawable = resources.getDrawable(R.drawable.ic_photo_library_black_24dp, resources.newTheme())
+                    this.currentSearchType = SERCH_TYPE.PHOTO
+                }
+                R.id.user_search_radio_btn -> {
+                    Log.d(TAG, "유저검색 버튼 클릭!")
+                    search_term_text_layout.hint = "사용자 검색"
+                    search_term_text_layout.startIconDrawable = resources.getDrawable(R.drawable.ic_person_black_24dp, resources.newTheme())
+                    this.currentSearchType = SERCH_TYPE.USER
+
+                }
+
+            }
+            Log.d(TAG,"MainActivity - onCheckedChange() called / currentSearchType : $currentSearchType")
+
+        }
+
+        // 텍스트가 변경이 되었을때
+        search_term_edit_text.onMyTextChange {
+            // 입력된 글자가 하나라도 있다면
+            if (it.toString().count() > 0) {
+                // 검색 버튼을 보여준다.
+                frame_search_btn.visibility = View.VISIBLE
+                search_term_text_layout.helperText = " "
+
+                // 스크롤뷰를 오린다.
+                main_scrllview.scrollTo(0, 300)
+            } else {
+                frame_search_btn.visibility = View.INVISIBLE
+            }
+
+            if (it.toString().count() == 12) {
+                Log.d(TAG,"MainActivity - 에러 띄우기")
+                Toast.makeText(this,"검색어는 12자 까지만 입력 가능합니다.", Toast.LENGTH_SHORT).show()
+
+            }
+        }
+
+        // 버튼 클릭시
+        btn_search.setOnClickListener {
+            Log.d(TAG,"MainActivity - 검색버튼이 클릭 되었다. / currentSearchType : $currentSearchType")
+            this.handleSearchButtonUi()
+        }
+
+    }// onCreate
+
+    private fun handleSearchButtonUi(){
+        btn_progress.visibility = View.VISIBLE
+
+        btn_search.text = ""
+
+        Handler().postDelayed({
+            btn_progress.visibility = View.INVISIBLE
+            btn_search.text = "검색"
+        }, 1500)
+    }
+}
---
+++ app/src/main/java/com/jnu/unsplash_app_tutorial/utlis/Constants.kt
@@ -0,0 +1,9 @@
+package com.jnu.unsplash_app_tutorial.utlis
+
+object Constants {
+    const val TAG : String = " 로그"
+}
+
+enum class SERCH_TYPE {
+    PHOTO, USER
+}
